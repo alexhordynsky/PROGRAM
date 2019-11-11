6723526a4ac340b59dcbc9c0387c4d4ca034037c
z18ista06 <z18ista06@Z00P1-17.SPSKNM>
2019-11-11 08:47:39 +0100

    Add project files.

---
+++ FOOTBALL TEAMS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FOOTBALL_TEAMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var defense = new Team();
+            defense.Defence();
+            var backup = new Team();
+            backup.Backup();
+            var attack = new Team();
+            attack.Attack();
+        }
+    }
+}
---
+++ FOOTBALL TEAMS/Team.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FOOTBALL_TEAMS
+{
+    class Team
+    {
+        public void Defence()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a <= 30 )
+            {
+                Console.WriteLine("Team has bad defense!");
+
+            }
+            else if (a < 80)
+            {
+                Console.WriteLine("Team have average defence!");
+            }
+            else if (a > 80)
+            {
+                Console.WriteLine("Team has great defense!");
+            }
+        }
+        public void Backup()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a <= 30)
+            {
+                Console.WriteLine("Team has low ball possesion!");
+
+            }
+            else if (a < 80)
+            {
+                Console.WriteLine("Team has average ball possesion!");
+            }
+            else if (a > 80)
+            {
+                Console.WriteLine("Team has great ball possesion!");
+            }
+
+        }
+        public void Attack()
+        {
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                if (a <= 30)
+                {
+                    Console.WriteLine("Team doesn´t score much goals!");
+
+                }
+                else if (a < 80)
+                {
+                    Console.WriteLine("Team scores average goals");
+                }
+                else if (a > 80)
+                {
+                    Console.WriteLine("Team scores much goals!");
+                }
+            }
+        }
+    }
+}
